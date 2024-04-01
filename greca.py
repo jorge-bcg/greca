68b189d19acb854f38ddfcd2129213622f4af376
Jorge A Barrios <jorge@barrios.group>
2024-04-01 13:41:45 -0400

    updated API key

--- greca.py
+++ greca.py
@@ -14,20 +14,20 @@ from shapely.ops import transform
 #from shapely import Point
 import pyproj
 import math
-from google.cloud import secretmanager
+#from google.cloud import secretmanager
 #import google-cloud-secret-manager as gsm
 
 ors_url = "https://api.openrouteservice.org/v2/matrix/driving-car"
 GC_API_URL = "https://api.goclimateneutral.org/v1/flight_footprint"
 
-#Make sure to run the following in the GCloud LI: gcloud auth application-default login
-project_id = "greca-418615"
-client = secretmanager.SecretManagerServiceClient()
-name = f"projects/{project_id}/secrets/GC_API_KEY/versions/1"
-response = client.access_secret_version(name=name)
-GC_API_KEY = response.payload.data.decode("UTF-8")
+# #Make sure to run the following in the GCloud LI: gcloud auth application-default login
+# project_id = "greca-418615"
+# client = secretmanager.SecretManagerServiceClient()
+# name = f"projects/{project_id}/secrets/GC_API_KEY/versions/1"
+# response = client.access_secret_version(name=name)
+# GC_API_KEY = response.payload.data.decode("UTF-8")
 
-print(GC_API_KEY)
+# print(GC_API_KEY)
 
 #First cut filtering by using a bounding box
 def filterCities(sel,a, buffer):
